63d02ff16a009dff1134c75d97c5fb71a3a914d0
shevyarora <shevykumar26@gmail.com>
2024-01-18 02:55:19 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+pencolor("Green")
+left(60)
+forward(120)
+pencolor("Red")
+
+right(120)
+forward(120)
+pencolor("Yellow")
+
+right(150)
+forward(120)
+pencolor("Orange")
+
+right(150)
+forward(120)
+pencolor("Blue")
+
+right(160)
+forward(150)
